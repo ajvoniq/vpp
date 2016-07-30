b51e2ca5d2b015ab0e93af4e673df8cd4b103575
Stefan Kobza <skobza@cisco.com>
2016-07-30 23:11:40 +0200

    Delete redundant tests.
    Signed-off-by: Stefan Kobza <skobza@cisco.com>

--- test/test_vxlan.py
+++ test/test_vxlan.py
@@ -102,17 +102,5 @@ class TestVxlan(unittest.TestCase):
         self.assertEqual(pkt[UDP].dport, 4789)
         self.assertEqual(str(pkt[Raw]), vxlan_header + str(payload))
 
-
-    def test_isupper(self):
-        self.assertTrue('FOO'.isupper())
-        self.assertFalse('Foo'.isupper())
-
-    def test_split(self):
-        s = 'hello world'
-        self.assertEqual(s.split(), ['hello', 'world'])
-        # check that s.split fails when the separator is not a string
-        with self.assertRaises(TypeError):
-            s.split(2)
-
 if __name__ == '__main__':
     unittest.main()
