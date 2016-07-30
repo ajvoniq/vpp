ff8a77edcdd440d20a4d9e0c05ce06e341a0b282
Stefan Kobza <skobza@cisco.com>
2016-07-30 22:52:48 +0200

    Move vxlan test to unittest format.
    Signed-off-by: Stefan Kobza <skobza@cisco.com>

---
+++ test/test_vxlan.py
@@ -0,0 +1,118 @@
+#!/bin/env python
+
+import unittest
+import os
+import socket
+
+import scapy
+import ipaddress
+from scapy.all import *
+
+import vpp_test
+from vpp_test import *
+
+class TestVxlan(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        cls.v = VppTest()
+        cls.v.create_links(2)
+
+        # Create VXLAN VTEP on pg0, and put pg0 and pg1 in BD
+        cls.v.cli(0, 'create vxlan tunnel src %s dst %s vni 1' %
+                (cls.v.VPP_IP4S[0], cls.v.MY_IP4S[0]))
+        cls.v.cli(0, 'set interface l2 bridge vxlan_tunnel0 1')
+        cls.v.cli(0, 'set interface l2 bridge pg1 1')
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.v.quit()
+
+    def tearDown(self):
+        v = self.__class__.v
+        v.cli(2, "show int")
+        v.cli(2, "show trace")
+        v.cli(2, "show hardware")
+        v.cli(2, "show ip arp")
+        v.cli(2, "show ip fib")
+        v.cli(2, "show error")
+        v.cli(2, "show run")
+        v.cli(2, "show bridge-domain 1 detail")
+
+    def test_decapBD(self):
+        v = self.__class__.v
+        pkts = []
+        payload = ( Ether(src='00:00:00:00:00:01', dst='00:00:00:00:00:02') /
+                IP(src='1.2.3.4', dst='4.3.2.1') /
+                UDP(sport=10000, dport=20000) / Raw('\x00' * 100))
+        p = ( Ether(src=v.MY_MACS[0], dst=v.VPP_MACS[0]) /
+                IP(src=v.MY_IP4S[0], dst=v.VPP_IP4S[0]) /
+                UDP(sport=4789, dport=4789, chksum=0) /
+                '\x08\x00\x00\x00'/'\x00\x00\x01\x00' /
+                payload)
+
+        pkts.append(p)
+        v.pg_arm(0, 1, pkts)
+        v.pg_send()
+
+        out = v.pg_read_output(1)
+        self.assertEqual(len(out), 1, 'Invalid number of packets on '
+                'output: %u' % len(out))
+
+        pkt = out[0]
+        # TODO: add error messages
+        self.assertEqual(pkt[Ether].src, payload[Ether].src)
+        self.assertEqual(pkt[Ether].dst, payload[Ether].dst)
+        self.assertEqual(pkt[IP].src, payload[IP].src)
+        self.assertEqual(pkt[IP].dst, payload[IP].dst)
+        self.assertEqual(pkt[UDP].sport, payload[UDP].sport)
+        self.assertEqual(pkt[UDP].dport, payload[UDP].dport)
+        self.assertEqual(pkt[Raw], payload[Raw])
+
+    def test_encapBD(self):
+        '''Packet coming from pg1 through BD to VTEP.
+        VXLAN encap is expected.
+        '''
+        v = self.__class__.v
+        pkts = []
+        vxlan_header = '\x08\x00\x00\x00' + '\x00\x00\x01\x00'
+        payload = ( Ether(src='00:00:00:00:00:01', dst='00:00:00:00:00:02') /
+                IP(src='1.2.3.4', dst='4.3.2.1') /
+                UDP(sport=10000, dport=20000) / Raw('\x00' * 100))
+        p = ( Ether(src=v.MY_MACS[0], dst=v.VPP_MACS[0]) /
+                IP(src=v.MY_IP4S[0], dst=v.VPP_IP4S[0]) /
+                UDP(sport=4789, dport=4789, chksum=0) /
+                vxlan_header /
+                payload)
+
+        pkts.append(payload)
+        v.pg_arm(1, 0, pkts)
+        v.pg_send()
+
+        out = v.pg_read_output(0)
+        self.assertEqual(len(out), 1, 'Invalid number of packets on '
+                'output: %u' % len(out))
+
+        pkt = out[0]
+        # TODO: add error messages
+        self.assertEqual(pkt[Ether].src, v.VPP_MACS[0])
+        self.assertEqual(pkt[Ether].dst, v.MY_MACS[0])
+        self.assertEqual(pkt[IP].src, v.VPP_IP4S[0])
+        self.assertEqual(pkt[IP].dst, v.MY_IP4S[0])
+        self.assertEqual(pkt[UDP].dport, 4789)
+        self.assertEqual(str(pkt[Raw]), vxlan_header + str(payload))
+
+
+    def test_isupper(self):
+        self.assertTrue('FOO'.isupper())
+        self.assertFalse('Foo'.isupper())
+
+    def test_split(self):
+        s = 'hello world'
+        self.assertEqual(s.split(), ['hello', 'world'])
+        # check that s.split fails when the separator is not a string
+        with self.assertRaises(TypeError):
+            s.split(2)
+
+if __name__ == '__main__':
+    unittest.main()
--- test/vpp_test.py
+++ test/vpp_test.py
@@ -10,7 +10,9 @@ from scapy.all import *
 
 
 class VppTest:
+
     def __init__(self):
+        self.pg_streams = []
         self.RED = '\033[91m'
         self.GREEN = '\033[92m'
         self.YELLOW = '\033[93m'
@@ -18,11 +20,65 @@ class VppTest:
         self.END = '\033[0m'
         self.vpp_bin = os.getenv('VPP_TEST_BIN', "vpp")
         self.vpp_api_test_bin = os.getenv ("VPP_TEST_API_TEST_BIN", "vpp-api-test")
+
+        self.MY_MACS = []
+        self.MY_IP4S = []
+        self.MY_IP6S = []
+        self.VPP_MACS = []
+        self.VPP_IP4S = []
+        self.VPP_IP6S = []
+
         try:
             self.verbose = int (os.getenv ("V", 0))
         except:
             self.verbose = 0
         self.vpp = subprocess.Popen([self.vpp_bin, "unix", "nodaemon"], stderr=subprocess.PIPE)
+
+    def create_links(self, num_if):
+        for i in range (0, num_if):
+            self.MY_MACS.append("00:00:00:00:ff:%02x" % i)
+            self.MY_IP4S.append("172.16.%u.2" % i)
+            self.VPP_IP4S.append("172.16.%u.1" % i)
+            self.log("My MAC address is %s, IPv4 address is %s" %
+                    (self.MY_MACS[i], self.MY_IP4S[i]))
+            self.cli(0, "create packet-generator interface pg%u" % i)
+            self.cli(0, "set interface state pg%u up" % i)
+            self.cli(0, "set interface ip address pg%u %s/24" %
+                    (i, self.VPP_IP4S[i]))
+
+        ###############################################################################
+        # Populate ARP table
+        #
+        ###############################################################################
+
+        # Prepare ARP requests for all interfaces
+        for i in range (0, num_if):
+            arp_req = ( Ether(dst="ff:ff:ff:ff:ff:ff",src=self.MY_MACS[i]) /
+                        ARP(op=ARP.who_has, pdst=self.VPP_IP4S[i],
+                            psrc=self.MY_IP4S[i], hwsrc=self.MY_MACS[i]))
+            self.pg_arm(i, i, arp_req)
+
+        self.cli(2, "trace add pg-input %u" % (num_if * 3))
+        self.pg_send()
+        self.cli(2, "show int")
+        self.cli(2, "show trace")
+        self.cli(2, "show hardware")
+        self.cli(2, "show ip arp")
+        self.cli(2, "show ip fib")
+        self.cli(2, "show error")
+
+        # Process replies, store VPP's MAC addresses
+        ok = 0
+        for i in range (0, num_if):
+            arp_reply = rdpcap("/tmp/pg%u_out.pcap" % i)[0]
+            if  arp_reply[ARP].op == ARP.is_at:
+                ok += 1
+                self.VPP_MACS.append(arp_reply[ARP].hwsrc)
+                self.log("VPP pg%u MAC address is %s " % ( i, self.VPP_MACS[i]))
+
+        if  ok != num_if:
+            raise RuntimeError('Number of ARP responses does not equal '
+                               'ARP requests')
 
     def test_suite(self, s):
         print self.YELLOW
@@ -66,6 +122,13 @@ class VppTest:
         wrpcap("/tmp/pg%u_in.pcap" % i, pkts)
         self.cli(0, "packet-generator new pcap /tmp/pg%u_in.pcap source pg%u name pcap%u" % (i, i, i))
         self.cli(0, "packet-generator capture pg%u pcap /tmp/pg%u_out.pcap" % (o, o))
+        self.pg_streams.append('pcap%u' % i)
+
+    def pg_send(self):
+        self.cli(0, 'packet-generator enable')
+        for stream in self.pg_streams:
+            self.cli(0, 'packet-generator delete %s' % stream)
+        self.pg_streams = []
 
     def pg_read_output(self, o):
         output = rdpcap("/tmp/pg%u_out.pcap" % o)
